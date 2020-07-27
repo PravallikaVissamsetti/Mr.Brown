90348fcd930757e5c4b2aee8b4be7a9f866f5f82
PravallikaVissamsetti <68841211+PravallikaVissamsetti@users.noreply.github.com>
2020-07-27 13:30:03 +0530

    Add files via upload

---
+++ build/precache-manifest.4560448cac12f296c15eb36a683683be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950b3a9b4db96c1b2f07d9326e690dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfb4a3555101f7da71e",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a543fa225aacd1711690",
+    "url": "/static/css/main.12642285.chunk.css"
+  },
+  {
+    "revision": "6bfb4a3555101f7da71e",
+    "url": "/static/js/2.9ec77178.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ec77178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a543fa225aacd1711690",
+    "url": "/static/js/main.d1ff3b00.chunk.js"
+  },
+  {
+    "revision": "b384d441a7d7802b1b5f",
+    "url": "/static/js/runtime-main.e96ec9c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
